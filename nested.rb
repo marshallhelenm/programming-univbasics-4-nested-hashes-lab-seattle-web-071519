af71d60b684f22fea684d722e5ebbe6b17a3984b
Helen Marshall <marshallhelenm@gmail.com>
2019-07-01 02:43:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+ }
+ return programmer_hash
     
 end
 
