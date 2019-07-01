471468e5916e460ffc0e4c1c4ddad9c4879fde0f
Helen Marshall <marshallhelenm@gmail.com>
2019-07-01 02:42:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     puts programmer_hash[:dennis_ritchie][:languages]
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
